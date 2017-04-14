3e3e022c0083e2f717da23df0e5a8a74449a5b67
glashunti <gleibsonglas2@yahoo.com.br>
2017-04-14 17:52:23 -0300

    Example from the book C++ Primer Plus
    Simple code to make calculations with floating-point operant and using
    some floating-point constants as float constant

---
+++ fltaddcpp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main()
+{
+
+	float a = 2.34E+22f;
+	float b = a + 1.0f;
+
+	std::cout << "a = " << a << "\n";
+	std::cout << "b - a = " << b - a << "\n";
+
+
+	return 0;
+
+}
